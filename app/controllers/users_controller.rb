0da9439640f2eaa86432be4c766d783393e067c3
amippy <aminaka1223@gmail.com>
2016-01-02 10:53:52 +0900

    add sign sign up and user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
-end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcom to the Sample Ami's app! "
+      redirect_to @user
     else
       render 'new'
     end
@@ -19,3 +24,4 @@ end
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
+end
